60368937e9b9afeae8cb29b23bfe28bbecbe1f81
Henri Remonen <henri@remonen.fi>
2023-03-07 13:13:18 +0200

    Create token utils

---
+++ src/server/utils/jwtGenerator.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"strconv"
+	"time"
+
+	"github.com/HRemonen/kanban-board/config"
+	"github.com/golang-jwt/jwt"
+)
+
+func GenerateNewAccessToken() (string, error) {
+	secret := config.Config("JWT_SECRET_KEY")
+
+	minutesCount, _ := strconv.Atoi(config.Config("JWT_SECRET_KEY_EXPIRE_MINUTES_COUNT"))
+
+	claims := jwt.MapClaims{}
+
+	claims["exp"] = time.Now().Add(time.Minute * time.Duration(minutesCount)).Unix()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	t, err := token.SignedString([]byte(secret))
+	if err != nil {
+		return "", err
+	}
+
+	return t, nil
+}
