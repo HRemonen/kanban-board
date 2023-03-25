7210e1f6b50d6a0776cb0d812985b1ca90ff64c4
Henri Remonen <henri@remonen.fi>
2023-03-25 13:50:39 +0200

    Added the AuthProvider to the App

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Login from './components/authentication/Login'
+import { AuthProvider } from './contexts/AuthContext'
 
 const App = () => (
   <section className="bg-slate-900 text-white">
-    <Routes>
-      <Route path="/" element={<Login />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </AuthProvider>
   </section>
 )
 
