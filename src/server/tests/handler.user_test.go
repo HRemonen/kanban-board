0a66fd57b8572e9d13fa096e2c123d4cbdff7b96
Henri Remonen <henri@remonen.fi>
2023-03-27 10:34:26 +0300

    More testing

--- src/server/tests/handler.user_test.go
+++ src/server/tests/handler.user_test.go
@@ -1,12 +1,15 @@
 package handlers
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/HRemonen/kanban-board/app/database"
 	"github.com/HRemonen/kanban-board/app/model"
+	"github.com/HRemonen/kanban-board/pkg/utils"
 	"github.com/HRemonen/kanban-board/setup"
 
 	"github.com/stretchr/testify/assert"
@@ -96,5 +99,83 @@ func TestGetAllUsers(t *testing.T) {
 		if user2Data["name"] != user2.Name || user2Data["email"] != user2.Email {
 			t.Errorf("Unexpected user data: %v", user2Data)
 		}
+	}
+}
+
+func TestGetSingleUser(t *testing.T) {
+	database.SetupTestDB()
+	app := setup.Setup()
+
+	db := database.DB.Db
+
+	hash, _ := utils.HashPassword("salainensalasana")
+
+	user1 := model.User{Name: "Alice", Email: "alice@example.com", Password: hash}
+	user2 := model.User{Name: "Bob", Email: "bob@example.com", Password: hash}
+
+	// Convert the user to bytes and the to io.Reader to pass to the NewRequest method
+	b, _ := json.Marshal(user1)
+
+	reader := bytes.NewReader(b)
+
+	// login to get the token
+	req := httptest.NewRequest(
+		"POST",
+		"/api/v1/auth/login",
+		reader,
+	)
+
+	res, _ := app.Test(req, -1)
+
+	fmt.Println(res)
+
+	db.Create(&user1)
+	db.Create(&user2)
+
+	tests := []struct {
+		description string
+
+		route string
+
+		expectedError   bool
+		expectedStatus  string
+		expectedCode    int
+		expectedMessage string
+	}{
+		{
+			description:     "get user by ID when authenticated succeeds",
+			route:           "/api/v1/user",
+			expectedError:   false,
+			expectedStatus:  "success",
+			expectedCode:    200,
+			expectedMessage: "User Found",
+		},
+		{
+			description:     "get user by ID when not authenticated fails",
+			route:           "/api/v1/user",
+			expectedError:   true,
+			expectedStatus:  "error",
+			expectedCode:    401,
+			expectedMessage: "Unauthorized action",
+		},
+	}
+
+	for _, test := range tests {
+		req := httptest.NewRequest(
+			"GET",
+			test.route,
+			nil,
+		)
+
+		res, err := app.Test(req, -1)
+
+		assert.Equalf(t, test.expectedError, err != nil, test.description)
+
+		if test.expectedError {
+			continue
+		}
+
+		assert.Equalf(t, test.expectedCode, res.StatusCode, test.description)
+
 	}
 }
