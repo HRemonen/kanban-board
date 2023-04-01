a1536eb003c9d6258e6645b402ffd3c31170708c
Henri Remonen <henri@remonen.fi>
2023-04-01 10:54:20 +0300

    Delete CheckAuthorization method

--- src/server/pkg/utils/checkAuthorization.go
+++ src/server/pkg/utils/checkAuthorization.go
@@ -3,15 +3,14 @@ package utils
 import (
 	"errors"
 
-	"github.com/HRemonen/kanban-board/app/database"
 	"github.com/HRemonen/kanban-board/app/model"
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/google/uuid"
 )
 
-// CheckAuthorization ... Check authorization of the user
-// @Summary Check authorization of the user
+// IsAuthorized ... Check authorization of the user
+// @Summary Check authorization of the user and return boolean and error if occured
 // @Description Checks if the user sending request is actually the user
 // @Description which holds the wanted resource.
 // @Tags Utils
@@ -29,33 +28,4 @@ func IsAuthorized(c *fiber.Ctx, user model.User) (bool, error) {
 	}
 
 	return true, nil
-}
-
-// CheckAuthorization ... Check authorization of the user
-// @Summary Check authorization of the user
-// @Description Checks if the user sending request is actually the user
-// @Description which holds the wanted resource.
-// @Tags Utils
-func CheckAuthorization(c *fiber.Ctx) (model.User, error) {
-	db := database.DB.Db
-
-	id := c.Params("id")
-
-	var user model.User
-
-	db.Find(&user, "id = ?", id)
-
-	if user.ID == uuid.Nil {
-		return user, errors.New("User not found")
-	}
-
-	authUser := c.Locals("user").(*jwt.Token)
-	claims := authUser.Claims.(jwt.MapClaims)
-	authUserId := claims["sub"].(string)
-
-	if user.ID.String() != authUserId {
-		return user, errors.New("Unauthorized action")
-	}
-
-	return user, nil
 }
