952ccffa4a5d051bdd531d3b86c5c146eec90dff
Henri Remonen <henri@remonen.fi>
2023-04-02 11:37:27 +0300

    Create auth service

--- src/server/app/services/service.auth.go
+++ src/server/app/services/service.auth.go
@@ -1,1 +1,49 @@
 package services
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/HRemonen/kanban-board/app/database"
+	"github.com/HRemonen/kanban-board/app/model"
+	"github.com/HRemonen/kanban-board/pkg/utils"
+	"github.com/gofiber/fiber/v2"
+)
+
+func Login(c *fiber.Ctx) (model.LoginData, error) {
+	db := database.DB.Db
+	payload := new(model.LoginUserInput)
+
+	c.BodyParser(payload)
+
+	var user model.User
+	var loginData model.LoginData
+	var validate = utils.NewValidator()
+
+	err := validate.Struct(payload)
+
+	if err != nil {
+		return loginData, err
+	}
+
+	err = db.First(&user, "email = ?", strings.ToLower(payload.Email)).Error
+
+	if err != nil {
+		return loginData, err
+	}
+
+	if !utils.CheckPasswordHash(payload.Password, user.Password) {
+		return loginData, errors.New("Invalid password")
+	}
+
+	if user.Provider == "Google" {
+		return loginData, errors.New("Use Oauth login for Google login")
+	}
+
+	token, err := utils.GenerateNewAccessToken(user.ID)
+
+	loginData.Token = token
+	loginData.User = model.FilteredResponse(&user)
+
+	return loginData, err
+}
