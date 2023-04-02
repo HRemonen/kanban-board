9b23f3d3678f5e54b0662982688aa03ec9e86ea1
Henri Remonen <henri@remonen.fi>
2023-04-02 11:41:17 +0300

    Create card service method for single card

--- src/server/app/services/service.card.go
+++ src/server/app/services/service.card.go
@@ -1,1 +1,18 @@
 package services
+
+import (
+	"github.com/HRemonen/kanban-board/app/database"
+	"github.com/HRemonen/kanban-board/app/model"
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetSingleCard(c *fiber.Ctx) (model.Card, error) {
+	db := database.DB.Db
+	var card model.Card
+
+	cardID := c.Params("id")
+
+	err := db.Model(&model.Card{}).Find(&card, "id = ?", cardID).Error
+
+	return card, err
+}
