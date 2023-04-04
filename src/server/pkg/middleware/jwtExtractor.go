38900881537e574a7b75ae885f383e6c9144164f
Henri Remonen <henri@remonen.fi>
2023-04-04 11:43:49 +0300

    Change the JWT middleware errors to be more precise

--- src/server/pkg/middleware/jwtExtractor.go
+++ src/server/pkg/middleware/jwtExtractor.go
@@ -20,17 +20,18 @@ func JWTProtected() func(*fiber.Ctx) error {
 }
 
 func jwtError(c *fiber.Ctx, err error) error {
-	// Return status 401 and failed authentication error.
 	if err.Error() == "Missing or malformed JWT" {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error":   true,
-			"message": err.Error(),
+		return c.Status(400).JSON(fiber.Map{
+			"status":  "error",
+			"message": "Missing or malformed JWT",
+			"data":    nil,
 		})
 	}
 
 	// Return status 401 and failed authentication error.
-	return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-		"error":   true,
-		"message": err.Error(),
+	return c.Status(401).JSON(fiber.Map{
+		"status":  "error",
+		"message": "Unauthorized action",
+		"data":    nil,
 	})
 }
