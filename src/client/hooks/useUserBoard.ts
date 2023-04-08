2a9b27171ffdd27cbe29c6eead2e76932d01b76e
Henri Remonen <henri@remonen.fi>
2023-04-08 14:53:31 +0300

    Create query for the user board based on the ID of the board

---
+++ src/client/hooks/useUserBoard.ts
@@ -0,0 +1,23 @@
+import { useQuery } from 'react-query'
+
+import apiClient from '../util/apiClient'
+import { config } from '../services/authService'
+import { APIResponse, Board } from '../types'
+
+const useUserBoard = (boardID: string) => {
+  const queryKey = ['board', boardID]
+
+  const query = async (): Promise<APIResponse> => {
+    const { data }: { data: APIResponse } = await apiClient.get(
+      `/board/${boardID}`,
+      config
+    )
+    return data
+  }
+
+  const { data: boardData, ...rest } = useQuery(queryKey, query)
+
+  return { boardData, ...rest }
+}
+
+export default useUserBoard
