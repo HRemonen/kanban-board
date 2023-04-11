3409c61ed65db6508cc00fe6074759bb87aaf712
Henri Remonen <henri@remonen.fi>
2023-04-11 17:55:06 +0300

    Create react query mutation hook for the location update

--- src/client/services/cardService.ts
+++ src/client/services/cardService.ts
@@ -1,3 +1,4 @@
+import { useMutation } from 'react-query'
 import apiClient from '../util/apiClient'
 import { APIResponse, Card, List } from '../types'
 
@@ -7,31 +8,40 @@ export const getSingleCard = async (id: string) => {
   return data
 }
 
-export const CreateListCard = async (list: List, card: Card) => {
+export const createListCard = async (listID: string, card: Card) => {
   const { data }: { data: APIResponse } = await apiClient.post(
-    `/list/${list.ID}/card`,
+    `/list/${listID}/card`,
     card
   )
 
   return data
 }
 
-export const UpdateListCardPosition = async (
-  list: List,
-  card: Card,
+export const useUpdateListCardPosition = () => {
+  const mutationFn = async ({ listID, cardID, position }: any) => {
+    await apiClient.put(`/list/${listID}/card/${cardID}`, { position })
+  }
+  const mutation = useMutation(mutationFn)
+
+  return mutation
+}
+
+export const updateListCardPosition = async (
+  listID: string,
+  cardID: string,
   position: number
 ) => {
   const { data }: { data: APIResponse } = await apiClient.post(
-    `/list/${list.ID}/card/${card.ID}`,
+    `/list/${listID}/card/${cardID}`,
     { position }
   )
 
   return data
 }
 
-export const DeleteListCard = async (list: List, card: Card) => {
+export const deleteListCard = async (listID: string, cardID: string) => {
   const { data }: { data: APIResponse } = await apiClient.delete(
-    `/list/${list.ID}/card/${card.ID}`
+    `/list/${listID}/card/${cardID}`
   )
 
   return data
