77b8cf4b3294bc9cde0bd5a2929f0d00dd71e8c2
Henri Remonen <henri@remonen.fi>
2023-04-15 10:31:46 +0300

    Render CardView elements from the listView component

--- src/client/components/views/ListsView.tsx
+++ src/client/components/views/ListsView.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Droppable, Draggable } from 'react-beautiful-dnd'
 
 import { Card, List } from '../../types'
+import CardView from './CardView'
 
 function sortCardsByPosition(list: List): void {
   list.Cards.sort((a: Card, b: Card) => a.Position - b.Position)
@@ -24,28 +25,7 @@ const ListView = ({ list }: { list: List }) => {
             ref={provided.innerRef}
           >
             {list.Cards.map((card) => (
-              <Draggable
-                key={card.ID}
-                draggableId={card.ID}
-                index={card.Position}
-              >
-                {(provided) => (
-                  <li
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                  >
-                    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
-                      <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900">
-                        {card.Title}
-                      </h5>
-                      <p className="mb-3 font-normal text-gray-500">
-                        {card.Description}
-                      </p>
-                    </div>
-                  </li>
-                )}
-              </Draggable>
+              <CardView key={card.ID} card={card} />
             ))}
             {provided.placeholder}
           </ul>
