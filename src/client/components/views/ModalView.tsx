36c385be93da404c0be824fa6f02b2af8f3048cd
Henri Remonen <henri@remonen.fi>
2023-04-21 09:53:35 +0300

    Update modal view

--- src/client/components/views/ModalView.tsx
+++ src/client/components/views/ModalView.tsx
@@ -6,57 +6,65 @@ type ModalProps = {
   setShowModal: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const ModalView = ({ card, setShowModal }: ModalProps) => (
-  <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-    <div className="relative w-full max-w-lg max-h-full">
-      <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-        <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-          <h3 className="text-2xl font-light">{card.Title}</h3>
-          <button
-            className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
-            type="button"
-            onClick={() => setShowModal(false)}
-          >
-            <svg
-              aria-hidden="true"
-              className="w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <span className="sr-only">Close modal</span>
-          </button>
+const ModalView = ({ card, setShowModal }: ModalProps) => {
+  console.log(card)
+  return (
+    <div className="fixed top-0 right-0 z-40 h-screen p-4 bg-white w-[60%] border-l-2">
+      <h5
+        id="drawer-label"
+        className="inline-flex items-center mb-6 text-base font-semibold text-gray-500 uppercase"
+      >
+        <svg
+          className="w-5 h-5 mr-2"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+            clipRule="evenodd"
+          />
+        </svg>
+        {card.Title}
+      </h5>
+      <button
+        type="button"
+        className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center"
+        onClick={() => setShowModal(false)}
+      >
+        <svg
+          aria-hidden="true"
+          className="w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+            clipRule="evenodd"
+          />
+        </svg>
+        <span className="sr-only">Close menu</span>
+      </button>
+      <div className="grid grid-cols-12 gap-2">
+        <div className="col-span-12">
+          <h3 className="mb-2 text-xl font-bold tracking-tight text-gray-900">
+            {card.Title}
+          </h3>
         </div>
-        <div className="p-6 space-y-6">
-          <p className="text-base leading-relaxed text-gray-500">
-            {card.Description}
+        <div className="col-span-7">
+          <p className="font-light text-gray-700">
+            {card.Description || 'No description provided'}
           </p>
         </div>
-        <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b">
-          <button
-            className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-            type="button"
-            onClick={() => setShowModal(false)}
-          >
-            Close
-          </button>
-          <button
-            className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-            type="button"
-            onClick={() => setShowModal(false)}
-          >
-            Save Changes
-          </button>
-        </div>
+        <div className="col-span-5 row-span-6">Info section</div>
+        <div className="col-span-7">Additional section (TBA)</div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default ModalView
