2b3a03c4ce21e6cee14cd7485664b4164396d7ed
Henri Remonen <henri@remonen.fi>
2023-04-21 08:45:51 +0300

    Update card styling

--- src/client/components/views/CardView.tsx
+++ src/client/components/views/CardView.tsx
@@ -1,25 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Draggable } from 'react-beautiful-dnd'
+import ModalView from './ModalView'
 
 import { Card } from '../../types'
 
-const CardView = ({ card }: { card: Card }) => (
-  <Draggable key={card.ID} draggableId={card.ID} index={card.Position}>
-    {(provided) => (
-      <li
-        ref={provided.innerRef}
-        {...provided.draggableProps}
-        {...provided.dragHandleProps}
-      >
-        <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
-          <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900">
-            {card.Title}
-          </h5>
-          <p className="mb-3 font-normal text-gray-500">{card.Description}</p>
-        </div>
-      </li>
-    )}
-  </Draggable>
-)
+const CardView = ({ card }: { card: Card }) => {
+  const [showModal, setShowModal] = useState(false)
+
+  return (
+    <>
+      <Draggable key={card.ID} draggableId={card.ID} index={card.Position}>
+        {(provided) => (
+          <li
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+          >
+            <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
+              <button
+                className="mb-2 text-md font-light tracking-tight text-gray-900 cursor-pointer hover:underline"
+                onClick={() => setShowModal(true)}
+                type="button"
+              >
+                {card.Title}
+              </button>
+            </div>
+          </li>
+        )}
+      </Draggable>
+      {showModal && <ModalView card={card} setShowModal={setShowModal} />}
+    </>
+  )
+}
 
 export default CardView
