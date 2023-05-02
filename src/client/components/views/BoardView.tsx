df02738946cdea11cee730d1275ad7d06b913a55
Henri Remonen <henri@remonen.fi>
2023-05-02 10:09:20 +0300

    If board is empty display text and button for creating first list

--- src/client/components/views/BoardView.tsx
+++ src/client/components/views/BoardView.tsx
@@ -6,13 +6,14 @@ import ListView from './ListsView'
 import useUserBoard from '../../hooks/useUserBoard'
 
 import { useUpdateCardPosition } from '../../services/cardService'
+import { useCreateNewList } from '../../services/listService'
 
 import { Board } from '../../types'
 import NewListView from './NewListView'
 
 const BoardView = () => {
   const { boardID: id } = useParams()
-
+  const mutateList = useCreateNewList()
   const mutateCardPosition = useUpdateCardPosition()
   const { boardData, isLoading } = useUserBoard(id)
 
@@ -34,6 +35,30 @@ const BoardView = () => {
     })
 
     return null
+  }
+
+  if (board.Lists.length === 0) {
+    const onClick = () => {
+      mutateList.mutateAsync({
+        boardID: board.ID,
+        list: { name: 'Todo list' },
+      })
+    }
+    return (
+      <div
+        data-cy={`empty-board-${board.ID}`}
+        className="px-6 text-black h-screen overflow-y-hidden"
+      >
+        <div className="flex flex-col justify-center items-center w-full h-full">
+          <h2 className="text-lg text-center">
+            This board is empty. Create a new list to get started.
+          </h2>
+          <button type="button" onClick={onClick} className="mt-6">
+            + Add New List
+          </button>
+        </div>
+      </div>
+    )
   }
 
   return (
