9f7afb8dd1ee00a473ba5f4c9b7b595b9be58fd2
Henri Remonen <henri@remonen.fi>
2023-05-07 13:29:26 +0300

    BoardCard styling

--- src/client/components/kanban/BoardCard.tsx
+++ src/client/components/kanban/BoardCard.tsx
@@ -13,45 +13,21 @@ const BoardCard = ({ board }: { board: Board }) => (
       >
         {board.Name}
       </Link>
-      <div className="flex space-x-2 text-gray-400 text-sm">
+      <div className="my-2 md:my-4 flex space-x-2 text-gray-400 text-sm">
         <svg
-          xmlns="http://www.w3.org/2000/svg"
           className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+          fill="currentColor"
+          stroke="none"
+          aria-hidden="true"
+          height="16"
+          viewBox="0 0 16 16"
+          version="1.1"
+          width="16"
+          data-view-component="true"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-          />
+          <path d="M0 1.75A.75.75 0 0 1 .75 1h4.253c1.227 0 2.317.59 3 1.501A3.743 3.743 0 0 1 11.006 1h4.245a.75.75 0 0 1 .75.75v10.5a.75.75 0 0 1-.75.75h-4.507a2.25 2.25 0 0 0-1.591.659l-.622.621a.75.75 0 0 1-1.06 0l-.622-.621A2.25 2.25 0 0 0 5.258 13H.75a.75.75 0 0 1-.75-.75Zm7.251 10.324.004-5.073-.002-2.253A2.25 2.25 0 0 0 5.003 2.5H1.5v9h3.757a3.75 3.75 0 0 1 1.994.574ZM8.755 4.75l-.004 7.322a3.752 3.752 0 0 1 1.992-.572H14.5v-9h-3.495a2.25 2.25 0 0 0-2.25 2.25Z" />
         </svg>
-        <p>{board.Description}</p>
-      </div>
-      <div className="flex space-x-2 text-gray-400 text-sm my-3">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-          />
-        </svg>
-        <p>1 Weeks Left</p>
+        <i>{board.Description || 'No desctiption provided'}</i>
       </div>
       <div className="flex justify-between border-t-2">
         <div className="my-2">
