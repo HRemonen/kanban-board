18195fa0e45bea1972a822939dd965d7502f9078
Henri Remonen <henri@remonen.fi>
2023-05-13 11:33:22 +0300

    Seed the database with ID's also for easier testing

--- src/server/tests/helpers/seedTestDB.go
+++ src/server/tests/helpers/seedTestDB.go
@@ -5,16 +5,17 @@ import (
 
 	"github.com/HRemonen/kanban-board/app/model"
 	"github.com/HRemonen/kanban-board/pkg/utils"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 func SeedTestUsers(db *gorm.DB) error {
 	hash, _ := utils.HashPassword("salainensalasana")
 
-	users := []model.User {
-		{Username: "Alice", Email: "alice@example.com", Password: hash},
-		{Username: "Bob", Email: "bob@example.com", Password: hash},
-		{Username: "Charlie", Email: "charlie@example.com", Password: hash},
+	users := []model.User{
+		{ID: uuid.MustParse("817c0ae8-f2ee-45c8-9e8f-d3bb2b42335d"), Username: "Alice", Email: "alice@example.com", Password: hash},
+		{ID: uuid.MustParse("d8f22bf2-3550-43cf-af53-85b71d69742c"), Username: "Bob", Email: "bob@example.com", Password: hash},
+		{ID: uuid.MustParse("b4ce4645-6da6-46d2-abb5-26aa55885d76"), Username: "Charlie", Email: "charlie@example.com", Password: hash},
 	}
 
 	err := db.Create(&users).Error
@@ -39,10 +40,11 @@ func SeedTestBoards(db *gorm.DB) error {
 	hash, _ := utils.HashPassword("salainen")
 
 	user := model.User{
-		Name: "",
-        Username: "Kaalebbi",
-        Email: "kale@kaalebbi.fi",
-        Password: hash,
+		ID:       uuid.MustParse("b9f56152-25e3-49a0-812e-a40c1ad431eb"),
+		Name:     "",
+		Username: "Kaalebbi",
+		Email:    "kale@kaalebbi.fi",
+		Password: hash,
 	}
 
 	err := db.Create(&user).Error
@@ -51,12 +53,12 @@ func SeedTestBoards(db *gorm.DB) error {
 		return fmt.Errorf("failed to seed test user data: %w", err)
 	}
 
-	boards := []model.Board {
-		{Name: "Project Alpha", Description: "Planning and execution of Alpha project", UserID: user.ID},
-		{Name: "Marketing Campaign",Description: "Ideas and tasks for our upcoming marketing campaign", UserID: user.ID},
-		{Name: "Team Building Activities", Description: "Planning and organizing team building activities for the company",  UserID: user.ID},
-		{Name: "Product Development", Description: "Tracking progress and tasks for our product development team",  UserID: user.ID},
-		{Name: "Bug Tracking",Description: "Tracking and fixing bugs for our software development projects", UserID: user.ID},
+	boards := []model.Board{
+		{ID: uuid.MustParse("a70d4aa4-d913-4b85-abbe-86271aaba147"), Name: "Project Alpha", Description: "Planning and execution of Alpha project", UserID: user.ID},
+		{ID: uuid.MustParse("7a02f5d7-75aa-46b7-a698-c073ce49b12f"), Name: "Marketing Campaign", Description: "Ideas and tasks for our upcoming marketing campaign", UserID: user.ID},
+		{ID: uuid.MustParse("4075b27e-ba24-400a-8f61-c2bc51fbf8cc"), Name: "Team Building Activities", Description: "Planning and organizing team building activities for the company", UserID: user.ID},
+		{ID: uuid.MustParse("bceb0bc4-aa05-430f-b543-cfd808d61427"), Name: "Product Development", Description: "Tracking progress and tasks for our product development team", UserID: user.ID},
+		{ID: uuid.MustParse("2ae97c29-46a4-4342-91f7-d552920b6578"), Name: "Bug Tracking", Description: "Tracking and fixing bugs for our software development projects", UserID: user.ID},
 	}
 
 	err = db.Create(&boards).Error
