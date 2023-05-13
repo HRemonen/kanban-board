0bd6ad8f61cbf616e9d339b3149a56eed62f5fef
Henri Remonen <henri@remonen.fi>
2023-05-13 16:00:03 +0300

    Acually test if the CI shit is testing this correctly and should fail now

--- src/server/tests/handler.list_test.go
+++ src/server/tests/handler.list_test.go
@@ -53,7 +53,7 @@ func TestCreateBoardList(t *testing.T) {
 			token:           user.Data.Token,
 			body:            `{"name": "Test list"}`,
 			expectedStatus:  "success",
-			expectedCode:    201,
+			expectedCode:    400,
 			expectedBody:    "",
 			expectedMessage: "A new list has been created",
 		},
